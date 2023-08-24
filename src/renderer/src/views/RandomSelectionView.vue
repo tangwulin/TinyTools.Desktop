1ba409d2503a33947be117bc46e057ec79903339
tangwulin <3498665850@qq.com>
2023-08-24 19:55:08 +0800

    随机抽选功能初步

--- src/renderer/src/views/RandomSelectionView.vue
+++ src/renderer/src/views/RandomSelectionView.vue
@@ -1,13 +1,43 @@
 <script setup>
-
+import { DiceOutline } from "@vicons/ionicons5";
 </script>
 
 <template>
-  <div>
-    <h1>RandomSelectionView</h1>
-  </div>
+  <n-layout style="height: calc(100% - 0.5rem); width: 100%">
+    <n-layout-content style="height: calc(100% - 1rem - 6rem)">
+      <div class="flex flex-row justify-center items-center h-full">
+        <n-p depth="3"> 还没有抽选…… </n-p>
+      </div>
+    </n-layout-content>
+    <n-layout-footer position="absolute">
+      <n-divider style="margin: 1rem 0" />
+      <div
+        style="height: 6rem"
+        class="flex flex-row justify-around items-center"
+      >
+        <div
+          style="margin: auto; width: 4rem; cursor: pointer"
+          class="flex flex-col justify-center items-center"
+        >
+          <n-icon size="2rem">
+            <DiceOutline />
+          </n-icon>
+          <span>快速开始</span>
+        </div>
+        <div
+          style="margin: auto; width: 4rem; cursor: pointer"
+          class="flex flex-col justify-center items-center"
+        >
+          <n-icon size="2rem">
+            <DiceOutline />
+          </n-icon>
+          <span>高级选项</span>
+        </div>
+      </div>
+    </n-layout-footer>
+  </n-layout>
+
+  <n-modal> </n-modal>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
