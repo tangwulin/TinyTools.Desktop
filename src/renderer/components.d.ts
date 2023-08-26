51eda1ab945e591812c36792ebd708b47ae7bc57
tangwulin <3498665850@qq.com>
2023-08-26 19:52:02 +0800

    自动导入

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -40,6 +40,7 @@ declare module 'vue' {
     NPopover: typeof import('naive-ui')['NPopover']
     NRadio: typeof import('naive-ui')['NRadio']
     NRadioGroup: typeof import('naive-ui')['NRadioGroup']
+    NResult: typeof import('naive-ui')['NResult']
     NScrollbar: typeof import('naive-ui')['NScrollbar']
     NSpace: typeof import('naive-ui')['NSpace']
     NStep: typeof import('naive-ui')['NStep']
