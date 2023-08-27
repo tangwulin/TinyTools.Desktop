16c4289c61c17337c9e5348ef436520891aab7c7
tangwulin <3498665850@qq.com>
2023-08-27 17:46:11 +0800

    值日表初步

--- src/renderer/src/views/Setup/WorkScheduleView.vue
+++ src/renderer/src/views/Setup/WorkScheduleView.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { ref, watch } from "vue";
 
+const hasImported = ref(false);
 </script>
 
 <template>
-
+  <n-card
+    style="margin: auto auto;display: flex;justify-content: center;align-items: center"
+    :bordered="false"
+  >
+    <n-table :single-line="false" v-if="hasImported">
+      <thead>
+      <tr>
+      </tr>
+      </thead>
+    </n-table>
+  </n-card>
 </template>
 
 <style scoped>
