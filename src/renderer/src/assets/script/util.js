41e0c82b40fe42d476388a863f247f85d3fff2c6
tangwulin <3498665850@qq.com>
2023-08-29 18:28:32 +0800

    增加无头像时的fallback

--- src/renderer/src/assets/script/util.js
+++ src/renderer/src/assets/script/util.js
@@ -1,3 +1,11 @@
 export function generateUniqueId() {
   return Date.now().toString(36) + Math.random().toString(36).slice(2);
 }
+
+export function remToPx(remValue)
+{
+  const baseFontSize = parseFloat(
+    getComputedStyle(document.documentElement).fontSize
+  );
+  return remValue * baseFontSize;
+}
