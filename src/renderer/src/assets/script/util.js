5fdfa858c0f7c1ee9527303fbfbc7d738e500d32
tangwulin <3498665850@qq.com>
2023-09-02 14:29:07 +0800

    增加通用文件下载方法

--- src/renderer/src/assets/script/util.js
+++ src/renderer/src/assets/script/util.js
@@ -13,3 +13,43 @@ export const remToPx = remValue => {
   const baseFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize)
   return remValue * baseFontSize
 }
+
+/**
+ * 使用blob中转，用于解决electron环境下下载不正常的问题
+ * @param {Request | string | URL} url
+ * @param {string?} filename
+ */
+export const downloadAnyFile = (url, filename) => fetch(url)
+  .then(async (res) => {
+    const blob = await res.blob()
+    // 如果未提供 filename，则从 URL 获取文件名
+    if (!filename)
+    {
+      const contentDisposition = res.headers.get('content-disposition')
+      if (contentDisposition && contentDisposition.indexOf('filename=') !== -1)
+      {
+        filename = contentDisposition
+          .split('filename=')[1]
+          .trim()
+          .replace(/["']/g, '') // 去掉可能存在的引号
+      }
+      else
+      {
+        // 如果无法从 Content-Disposition 获取文件名，尝试从 URL 获取
+        const urlParts = url.split('/')
+        filename = urlParts[urlParts.length - 1]
+      }
+    }
+
+    const fileURL = URL.createObjectURL(blob)
+
+    const a = document.createElement('a')
+    a.download = filename
+    a.href = fileURL
+    document.body.appendChild(a)
+    a.click()
+    a.remove()
+  })
+  .catch((err) => {
+    console.error(err)
+  })
