8a14bf4126fc6b35d29a93b6c333f33be2af6fb5
tangwulin <3498665850@qq.com>
2023-09-02 14:28:51 +0800

    增加jsdoc

--- src/renderer/src/assets/script/util.js
+++ src/renderer/src/assets/script/util.js
@@ -1,12 +1,15 @@
-export function generateUniqueId()
-{
-  return Date.now().toString(36) + Math.random().toString(36).slice(2)
-}
+/**
+ * 就像它名字所描述的那样
+ * @returns {string}
+ */
+export const generateUniqueId = () => Date.now().toString(36) + Math.random().toString(36).slice(2)
 
-export function remToPx(remValue)
-{
-  const baseFontSize = parseFloat(
-    getComputedStyle(document.documentElement).fontSize,
-  )
+/**
+ * 就像它名字所描述的那样
+ * @param {number} remValue
+ * @returns {number}
+ */
+export const remToPx = remValue => {
+  const baseFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize)
   return remValue * baseFontSize
 }
