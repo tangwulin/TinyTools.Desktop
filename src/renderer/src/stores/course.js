2b32ef86124692d483b6766235b4f6d9b1f94c94
tangwulin <3498665850@qq.com>
2023-11-27 13:18:51 +0800

    变基后的灾难

--- src/renderer/src/stores/course.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
-export const useCourseStore = defineStore('course', () => {
-  const allCourses = ref([])
-
-  return { allCourses }
-}, { persist: true })
