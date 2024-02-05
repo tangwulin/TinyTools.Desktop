e48b5d41bbd9ebcf1521b70b6283aa1eb02a583e
tangwulin <3498665850@qq.com>
2024-02-05 21:47:56 +0800

    feat:add file icon extractor

--- src/main/utils/fsUtil.ts
+++ src/main/utils/fsUtil.ts
@@ -1,6 +1,6 @@
-import { execSync } from 'child_process'
+import fileIcon from 'extract-file-icon-fix'
 import fs from 'fs'
-import path, { join } from 'path'
+import path from 'path'
 
 export const getFilesAndFoldersInDir = (dir: string) => {
   return fs.promises.readdir(dir).then((files) => {
@@ -16,27 +16,44 @@ export const getFilesAndFoldersInDir = (dir: string) => {
   })
 }
 
-export const getFileIcon = (filePath: string) => {
-  const cachesPath = path.resolve(__dirname, '../../caches')
-  return execSync(
-    join(__dirname, '../../resources/bin/ExtractPngOfExtIconCpp.exe') +
-      ` -file ${filePath}` +
-      ` -O ${cachesPath}`
-  ).toString()
-}
-
-export const getFileIconByCache = (filePath: string) => {
+export const getFileIconByCache = async (filePath: string) => {
   const ext = path.extname(filePath)
+  console.log('ext', ext)
   if (isPicture(ext)) {
-    return filePath //TODO
-  } else {
+    const stats = await fs.promises.stat(filePath)
+    //大小小于10M的图片直接返回
+    if (stats.size < 1024 * 1024 * 10) {
+      return filePath
+    }
+
+    //大于10M的图片返回统一的图标
     const cachesPath = path.resolve(__dirname, '../../caches')
     const iconPath = `${cachesPath}/${ext}.png`
-    if (!fs.existsSync(iconPath) || ext === 'exe' || ext === 'lnk') {
-      return getFileIcon(filePath)
-    } else {
+    if (!fs.existsSync(iconPath)) {
+      const iconBuffer = fileIcon(filePath, 256)
+      await fs.promises.writeFile(iconPath, iconBuffer)
+    }
+    return iconPath
+  } else {
+    const cachesPath = path.resolve(__dirname, '../../caches')
+    //exe和lnk文件返回自己的图标
+    if (ext === '.exe' || ext === '.lnk') {
+      const fileName = path.basename(filePath)
+      const iconPath = `${cachesPath}/${fileName}.png`
+      if (!fs.existsSync(iconPath)) {
+        const iconBuffer = fileIcon(filePath, 256)
+        await fs.promises.writeFile(iconPath, iconBuffer)
+      }
       return iconPath
     }
+
+    //其余照常
+    const iconPath = `${cachesPath}/${ext}.png`
+    if (!fs.existsSync(iconPath)) {
+      const iconBuffer = fileIcon(filePath, 256)
+      await fs.promises.writeFile(iconPath, iconBuffer)
+    }
+    return iconPath
   }
 }
 
